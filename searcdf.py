ba0eb013cb221f78033357e65249af2ae9e84ad4
Rudratej Tiwari <45898060+rudra98@users.noreply.github.com>
2019-03-07 19:22:10 +0530

    Update searcdf.py

--- searcdf.py
+++ searcdf.py
@@ -1,16 +1,19 @@
-# -*- coding: utf-8 -*-
 """
 Created on Mon Dec 31 10:41:38 2018
-
-@author: dubek
 """
+#importing pandas library
 import pandas as pd
 
+#reading csv file
 df = pd.read_csv('C:/Users/dubek/Downloads/Folder/ecommerce_data.csv')
 
+#accessing the required data column using iloc function
+#working of iloc:
+#.iloc[index of starting row : index of ending row+1, index of starting column: index of ending column+1] 
 var1 = df.iloc[0:,2:3].values
-#print(var1)
+
 count = 0
+#couting number of values in the required range
 for var in var1:
     if var > 0.5 and var < 0.6:
         count = count + 1
